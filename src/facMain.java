84bb63f82cc58fbf97e2e4920bbb57c984e10f9a
XeroxBoy <827312773@qq.com>
2017-11-08 09:40:05 +0800

    123

--- src/facMain.java
+++ src/facMain.java
@@ -9,29 +9,43 @@ import java.util.Scanner;
  */
 public class facMain {
  public static void main(String[] args) {
-     Scanner sc=new Scanner(System.in);
-     String createWord=sc.nextLine();
-     characterFac factory;
-     character character=null;
-     switch(createWord)
-     {
-         case "architect":factory=new architectFac();
-          character = factory.createCharacter();break;
-         case "cook":factory=new cookFac();
-             character=factory.createCharacter();break;
-         case "doctor":factory=new doctorFac();
-             character=factory.createCharacter();break;
-         case "goldworker":factory=new goldworkerFac();
-             character=factory.createCharacter();break;
-         case "magician":factory=new magicianFac();
-             character=factory.createCharacter();break;
-         case "technician":factory=new technicianFac();
-             character=factory.createCharacter();break;
-         default:break;
+     while (true) {
+         Scanner sc = new Scanner(System.in);
+         String createWord = sc.nextLine();
+         characterFac factory;
+         character character = null;
+         switch (createWord) {
+             case "architect":
+                 factory = new architectFac();
+                 character = factory.createCharacter();
+                 break;
+             case "cook":
+                 factory = new cookFac();
+                 character = factory.createCharacter();
+                 break;
+             case "doctor":
+                 factory = new doctorFac();
+                 character = factory.createCharacter();
+                 break;
+             case "goldworker":
+                 factory = new goldworkerFac();
+                 character = factory.createCharacter();
+                 break;
+             case "magician":
+                 factory = new magicianFac();
+                 character = factory.createCharacter();
+                 break;
+             case "technician":
+                 factory = new technicianFac();
+                 character = factory.createCharacter();
+                 break;
+             default:
+                 break;
+         }
+         if (character != null)
+             character.say();
+         else
+             System.out.print("输入有误");
      }
-     if(character!=null)
-     character.say();
-     else
-         System.out.print("输入有误");
-    }
+ }
     }
--- src/simpleBean/people.java
+++ src/simpleBean/people.java
@@ -3,7 +3,7 @@ package simpleBean;
 /**
  * Created by AlexAnderIch on 2017/11/6.
  */
-public class People {
+public abstract class People {
     public void say(){
         System.out.println("我是人类");
     }
